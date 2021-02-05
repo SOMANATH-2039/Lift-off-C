ed5d2e03f1d7620ae5252860097a352675910aa0
SOMANATH-2039 <78495712+SOMANATH-2039@users.noreply.github.com>
2021-02-05 21:33:45 +0530

    Create Q1.c

---
+++ Week-1/Q1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+	float a,b,c,i,d,j;	printf("enter first number:");
+
+	scanf("%f",&i);
+
+	printf("enter second number:");
+
+	scanf("%f",&j);
+
+	a=i+j;
+
+	b=i-j;
+
+	c=i*j;
+
+	d=i/j;
+
+	printf("addition :%f \n",a);
+
+	printf("subtraction :%f \n",b);
+
+	printf("mutipplication :%f \n",c);
+
+	printf("division :%f \n",d);
+
+	
+
+}
