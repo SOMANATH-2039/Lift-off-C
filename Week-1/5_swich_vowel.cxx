6d49bfe7f68e1e5dab51de1a9284c5f168588660
SOMANATH-2039 <78495712+SOMANATH-2039@users.noreply.github.com>
2021-02-08 14:46:25 +0530

    Add files via upload

---
+++ Week-1/5_swich_vowel.cxx
@@ -0,0 +1,45 @@
+#include <stdio.h>
+void main()
+{
+	char ch;
+	printf("Enter an english alphabet:");
+	scanf("%s",&ch);
+	switch (ch)
+	{
+		case 'a':
+		printf("This is a vowel");
+		break;
+		case 'e':
+		printf("This is a vowel");
+		break;
+		case 'i':
+		printf("This is a vowel");
+		break;
+		case 'o':
+		printf("This is a vowel");
+		break;
+		case 'u':
+		printf("This is a vowel");
+		break;
+		case 'A':
+		printf("This is a vowel");
+		break;
+		case 'E':
+		printf("This is a vowel");
+		break;
+		case 'I':
+		printf("This is a vowel");
+		break;
+		case 'O':
+		printf("This is a vowel");
+		break;
+		case 'U':
+		printf("This is a vowel");
+		break;
+		
+		default:
+		printf("this is a consonat");
+		break;
+		
+	}
+}
